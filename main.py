d1285526b1f628a799b6156912f81c6651b7338c
EIZK3RN <jbettenh@gmail.com>
2017-02-11 09:05:37 -0500

    chapter 02 url requests with json

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-import kivy
-kivy.require("1.9.2")
+from kivy.network.urlrequest import UrlRequest
+import json
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+#kivy.require("1.9.2")
 
 class AddLocationForm(BoxLayout):
     search_input = ObjectProperty()
+    search_results = ObjectProperty()
 
     def search_location(self):
-        print(kivy.__version__)
+        search_template = "http://api.openweathermap.org/data/2.5/" + "findq={}&type=like"
+        search_url = search_template.format(self.search_input.text)
+        request = UrlRequest(search_url, self.found_location)
 
+    def found_location(self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data
+        cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in data['list']]
+        self.search_results.item_strings = cities
 
 class WeatherApp(App):
     pass
